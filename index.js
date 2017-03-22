52cec2d51e5873cabfcb9c46da2b2b6717cce404
rcolepeterson <cole.peterson@possible.com>
2017-03-21 20:22:24 -0700

    removed firbase calls

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 // https://www.ibm.com/watson/developercloud/tone-analyzer/api/v3/#authentication
 // https://github.com/watson-developer-cloud/tone-analyzer-nodejs/blob/master/app.js
+
 /*
+ 
  curl -u "4bebb53b-cad2-496c-9b2b-cb1510285bc7":"2jgXp1Im8RMk" -H "Content-Type: application/json" -d "{\"text\": \"Hi Team, I know the times are difficult! Our sales have been disappointing for the past three quarters for our data analytics product suite. We have a competitive data analytics product suite in the industry. But we need to do our job selling it! \"}" "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19"
+
 */
 
-var dotenv = require('dotenv-extended').load();
-var watson = require('watson-developer-cloud');
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const dotenv = require('dotenv-extended').load();
+const watson = require('watson-developer-cloud');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
 
 var tone_analyzer = watson.tone_analyzer({
   username: process.env.USER_NAME,
@@ -20,25 +23,26 @@ var tone_analyzer = watson.tone_analyzer({
   version_date: '2016-05-19'
 });
 
+var Client = require('node-rest-client').Client;
+var client = new Client();
 
-
-app.get('/analyze', (req, res) => {
-  res.send('analyzed');
+app.get('/', (req, res) => {
+  res.send('tone analyzer');
 });
 
 // curl -H "Content-Type: application/json" -X POST -d '{"text":"Why are here and why are we ding this?"}' http://localhost:3000/analyzeThis
-app.post('/analyzeThis', function (req, res) {
+app.post('/analyzeThis',  (req, res) => {
+  
   var input = req.body.input || req.body;
   tone_analyzer.tone({ 
   text: input.text,
   tone: 'emotion',
   sentences:false
  },
-  function(err, tone) {
+  (err, tone) => {
     if (err)
       console.log(err);
     else
-      console.log(JSON.stringify(tone, null, 2));
       var resObject = JSON.stringify(tone, null, 2);
       res.send(resObject);
   });
@@ -47,6 +51,16 @@ app.post('/analyzeThis', function (req, res) {
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+
+const updateFireBase = () => {
+
+jsonClient.get('/messages')
+  .then(res => {
+    console.log('got')
+  });
+
+}
 
 
 /*
