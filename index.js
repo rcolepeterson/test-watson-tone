17ff9d3e5424a54d3fdefcb0257b9589fd717ab9
rcolepeterson <cole.peterson@possible.com>
2017-03-22 19:16:48 -0700

    removed firebase call. Simplified port definition

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-// https://www.ibm.com/watson/developercloud/tone-analyzer/api/v3/#authentication
-// https://github.com/watson-developer-cloud/tone-analyzer-nodejs/blob/master/app.js
-
-/*
- 
- curl -u "4bebb53b-cad2-496c-9b2b-cb1510285bc7":"2jgXp1Im8RMk" -H "Content-Type: application/json" -d "{\"text\": \"Hi Team, I know the times are difficult! Our sales have been disappointing for the past three quarters for our data analytics product suite. We have a competitive data analytics product suite in the industry. But we need to do our job selling it! \"}" "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19"
-
-*/
-
 const dotenv = require('dotenv-extended').load();
 const watson = require('watson-developer-cloud');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const port = process.env.port || 3000;
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -23,11 +15,8 @@ var tone_analyzer = watson.tone_analyzer({
   version_date: '2016-05-19'
 });
 
-var Client = require('node-rest-client').Client;
-var client = new Client();
-
 app.get('/', (req, res) => {
-  res.send('tone analyzer');
+  res.send('You have reached the Tone Analyzer');
 });
 
 // curl -H "Content-Type: application/json" -X POST -d '{"text":"Why are here and why are we ding this?"}' http://localhost:3000/analyzeThis
@@ -35,44 +24,16 @@ app.post('/analyzeThis',  (req, res) => {
   
   var input = req.body.input || req.body;
   tone_analyzer.tone({ 
-  text: input.text,
-  tone: 'emotion',
-  sentences:false
+  text: input.text
  },
   (err, tone) => {
     if (err)
       console.log(err);
     else
-      var resObject = JSON.stringify(tone, null, 2);
-      res.send(resObject);
+      res.send(JSON.stringify(tone, null, 2));
   });
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(port, () => {
   console.log('Example app listening on port 3000!');
 });
-
-
-const updateFireBase = () => {
-
-jsonClient.get('/messages')
-  .then(res => {
-    console.log('got')
-  });
-
-}
-
-
-/*
-tone_analyzer.tone({ 
-  text: 'A word is dead when it is said, some say. Emily Dickinson',
-  tone: 'emotion',
-  sentences:false
- },
-  function(err, tone) {
-    if (err)
-      console.log(err);
-    else
-      console.log(JSON.stringify(tone, null, 2));
-});
-*/
