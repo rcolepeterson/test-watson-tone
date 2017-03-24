e880bfad30a045951f424d96c3d5557fd7c3c762
rcolepeterson <cole.peterson@possible.com>
2017-03-24 12:39:12 -0700

    code cleanup

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const dotenv = require('dotenv-extended').load();
 const watson = require('watson-developer-cloud');
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.port || 3000;
 const errors = require('throw.js');
+const app = express();
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-var tone_analyzer = watson.tone_analyzer({
+const tone_analyzer = watson.tone_analyzer({
   username: process.env.USER_NAME,
   password: process.env.PASSWORD,
   version: 'v3',
@@ -32,11 +32,10 @@ app.get('/', (req, res) => {
 // curl -H "Content-Type: application/json" -X POST -d '{"text":"Why are here and why are we ding this?"}' http://localhost:3000/analyzeThis
 app.post('/analyzeThis',  (req, res, next) => {
   
-  var input = req.body.input || req.body;
-  
-  if (!input.text){
+  let input = req.body.input || req.body;
+ 
+  if (!input.text)
     next(new errors.NotAcceptable('No input. expecting a property named text holding a string'));
-  }
   
   tone_analyzer.tone({ 
   text: input.text
